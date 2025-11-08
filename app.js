488fb1c33ae9f0c4db90fbf09501513f46dcf64f
Mai Tran Ngoc Tran <tranB2207572@student.ctu.edu.vn>
2025-11-08 09:29:55 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -10,11 +10,26 @@ app.use(express.json());
 
 // Routes
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to contact book application." });
+  res.json({ message: "Welcome to the backend server!" });
 });
 
 // Routes
 const contactRoutes = require("./app/routes/contact.route");
 app.use("/api/contacts", contactRoutes);
+
+const ApiError = require("./app/api-error");
+
+// Middleware handle 404
+app.use((req, res, next) => {
+  // Nếu không khớp route nào
+  return next(new ApiError(404, "Resource not found"));
+});
+
+// Middleware xử lý lỗi cuối cùng
+app.use((err, req, res, next) => {
+  return res.status(err.statusCode || 500).json({
+    message: err.message || "Internal Server Error",
+  });
+});
 
 module.exports = app;
---
+++ app/api-error.js
@@ -0,0 +1,8 @@
+class ApiError extends Error {
+  constructor(statusCode, message) {
+    super(message);
+    this.statusCode = statusCode;
+    this.name = "ApiError";
+  }
+}
+module.exports = ApiError;
